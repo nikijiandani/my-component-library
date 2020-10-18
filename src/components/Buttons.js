20dcc76ced0cacec19ad54be6c13f8f211a4790f
Niki Jiandani <nikita.jiandani@caseware.com>
2020-10-18 15:42:15 -0400

    Add and toggle between default & dark theme

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { defaultTheme, typeScale, primaryFont } from "../utils";
+import { typeScale } from "../utils";
 import { applyStyleModifiers } from "styled-components-modifiers";
 
 const BUTTON_MODIFIERS = {
@@ -8,107 +8,83 @@ const BUTTON_MODIFIERS = {
     padding: 8px;
   `,
   large: () => `
-    font-size: ${typeScale.h5};
+    font-size: ${typeScale.header5};
     padding: 16px 24px;
   `,
   warning: () => `
-    background: none;
-    color: ${defaultTheme.status.warningColor};
+    background-color: ${(props) => props.theme.status.warningColor};
+    color: ${(props) => props.theme.textColorInverted};
     
     &:hover, &:focus {
-      background-color: ${defaultTheme.status.warningColorHover};
-      outline: 3px solid ${defaultTheme.status.warningColorHover};
+      background-color: ${(props) => props.theme.status.warningColorHover};
+      outline: 3px solid ${(props) => props.theme.status.warningColorHover};
       outline-offset: 2px;
-      border: 2px solid transparent;
     }
 
     &:active {
-      background-color: ${defaultTheme.status.warningColorActive};
+      background-color: ${(props) => props.theme.status.warningColorActive};
     }
-  `,
-  primaryButtonWarning: () => `
-    background-color: ${defaultTheme.status.warningColor};
-    color: ${defaultTheme.textColorInverted};
-  `,
-  secondaryButtonWarning: () => `
-    border: 2px solid ${defaultTheme.status.warningColor};
   `,
   error: () => `
-    background: none;
-    color: ${defaultTheme.status.errorColor};
-    &:hover, &:focus {
-      background-color: ${defaultTheme.status.errorColorHover};
-      outline: 3px solid ${defaultTheme.status.errorColorHover};
-      outline-offset: 2px;
-      border: 2px solid transparent;
-    }
-    &:active {
-      background-color: ${defaultTheme.status.errorColorActive};
-    }
-  `,
-  primaryButtonError: () => `
-    background-color: ${defaultTheme.status.errorColor};
-    color: ${defaultTheme.textColorInverted};
-  `,
-  secondaryButtonError: () => `
-    border: 2px solid ${defaultTheme.status.warningColor};
+  background-color: ${(props) => props.theme.status.errorColor};
+  color: ${(props) => props.theme.textColorInverted};
+
+  &:hover {
+    background-color: ${(props) => props.theme.status.errorColorHover};
+  }
+
+  &:active {
+    background-color: ${(props) => props.theme.status.errorColorActive};
+  }
   `,
   success: () => `
-    background: none;
-    color: ${defaultTheme.status.successColor};
-    &:hover, &:focus {
-      background-color: ${defaultTheme.status.successColorHover};
-      outline: 3px solid ${defaultTheme.status.successColorHover};
-      outline-offset: 2px;
-      border: 2px solid transparent;
-    }
-    &:active {
-      background-color: ${defaultTheme.status.successColorActive};
-    }
-  `,
-  primaryButtonSuccess: () => `
-    background-color: ${defaultTheme.status.successColor};
-    color: ${defaultTheme.textColorInverted};
-  `,
-  secondaryButtonSuccess: () => `
-    border: 2px solid ${defaultTheme.status.warningColor};
+  background-color: ${(props) => props.theme.status.successColor};
+  color: ${(props) => props.theme.textColorInverted};
+
+  &:hover {
+    background-color: ${(props) => props.theme.status.successColorHover};
+  }
+
+  &:active {
+    background-color: ${(props) => props.theme.status.successColorActive};
+  }
   `,
 };
 
 const Button = styled.button`
-  padding: 12px 24px;
+  padding: 8px 12px;
   font-size: ${typeScale.paragraph};
   border-radius: 2px;
   min-width: 100px;
   cursor: pointer;
-  font-family: ${primaryFont};
+  font-family: ${(props) => props.theme.primaryFont};
   transition: background-color 0.2s linear, color 0.2s linear;
 
   &:hover {
-    background-color: ${defaultTheme.primaryHoverColor};
-    color: ${defaultTheme.textColorOnPrimary};
+    background-color: ${(props) => props.theme.primaryHoverColor};
+    color: ${(props) => props.theme.textColorOnPrimary};
   }
 
   &:focus {
-    outline: 3px solid ${defaultTheme.primaryHoverColor};
+    outline: 3px solid ${(props) => props.theme.primaryHoverColor};
     outline-offset: 2px;
   }
 
   &:active {
-    background-color: ${defaultTheme.primaryActiveColor};
-    border-color: ${defaultTheme.primaryActiveColor};
-    color: ${defaultTheme.textColorOnPrimary};
+    background-color: ${(props) => props.theme.primaryActiveColor};
+    border-color: ${(props) => props.theme.primaryActiveColor};
+    color: ${(props) => props.theme.textColorOnPrimary};
   }
 `;
 
-const PrimaryButton = styled(Button)`
-  background-color: ${defaultTheme.primaryColor};
-  border: none;
-  color: white;
+export const PrimaryButton = styled(Button)`
+  background-color: ${(props) => props.theme.primaryColor};
+  border: 2px solid transparent;
+  color: ${(props) => props.theme.textColorOnPrimary};
 
   &:disabled {
-    background-color: ${defaultTheme.disabled};
-    color: ${defaultTheme.textOnDisabled};
+    background-color: ${(props) => props.theme.disabled};
+    color: ${(props) => props.theme.textOnDisabled};
     cursor: not-allowed;
   }
 
@@ -116,32 +92,27 @@ const PrimaryButton = styled(Button)`
 `;
 
 export const SecondaryButton = styled(Button)`
+  border: 2px solid ${(props) => props.theme.primaryColor};
+  color: ${(props) => props.theme.primaryColor};
   background: none;
-  border: 2px solid ${defaultTheme.primaryColor};
-  color: ${defaultTheme.primaryColor};
 
   &:disabled {
     background: none;
-    color: ${defaultTheme.disabled};
+    border: 2px solid ${(props) => props.theme.disabled};
+    color: ${(props) => props.theme.disabled};
     cursor: not-allowed;
-    border-color: ${defaultTheme.disabled};
   }
-
   ${applyStyleModifiers(BUTTON_MODIFIERS)}
 `;
 
 export const TertiaryButton = styled(Button)`
+  border: 2px solid transparent;
+  color: ${(props) => props.theme.primaryColor};
   background: none;
-  border: none;
-  color: ${defaultTheme.primaryColor};
 
   &:disabled {
-    background: none;
-    color: ${defaultTheme.disabled};
+    color: ${(props) => props.theme.disabled};
     cursor: not-allowed;
   }
-
   ${applyStyleModifiers(BUTTON_MODIFIERS)}
 `;
-
-export default PrimaryButton;
--- src/index.js
+++ src/index.js
@@ -1,27 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
-import PrimaryButton, {
+import { ThemeProvider } from "styled-components";
+import {
+  PrimaryButton,
   SecondaryButton,
   TertiaryButton,
 } from "./components/Buttons";
-import { GlobalStyle } from "./utils";
+import { GlobalStyle, darkTheme, defaultTheme } from "./utils";
 
 const App = () => {
+  const [useDarkTheme, setUseDarkTheme] = useState(false);
+
   return (
-    <div>
-      <PrimaryButton modifiers={["small", "error", "primaryButtonError"]}>
-        Hello world
-      </PrimaryButton>
-      <SecondaryButton
-        modifiers={["large", "warning", "secondaryButtonWarning"]}
+    <ThemeProvider theme={useDarkTheme ? darkTheme : defaultTheme}>
+      <button
+        style={{ margin: "0 16px 24px", padding: "8px", background: "none" }}
+        onClick={() => setUseDarkTheme(true)}
       >
-        Goodbye world
-      </SecondaryButton>
-      <TertiaryButton modifiers={["success", "tertiaryButtonSuccess"]}>
-        Goodbye world
-      </TertiaryButton>
+        Dark Theme
+      </button>
+      <button
+        style={{ margin: "0 16px 24px", padding: "8px", background: "none" }}
+        onClick={() => setUseDarkTheme(false)}
+      >
+        Default Theme
+      </button>
+
+      <div
+        style={{
+          background: useDarkTheme
+            ? defaultTheme.primaryColor
+            : darkTheme.primaryColor,
+          width: "100vw",
+          height: "100vh",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-around",
+        }}
+      >
+        <PrimaryButton>Hello world</PrimaryButton>
+        <SecondaryButton>Goodbye world</SecondaryButton>
+        <TertiaryButton>Hey world</TertiaryButton>
+      </div>
       <GlobalStyle />
-    </div>
+    </ThemeProvider>
   );
 };
 
