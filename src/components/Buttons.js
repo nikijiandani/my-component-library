5f8387c55d48d24953f477b9880dd2020843c216
Niki Jiandani <nikita.jiandani@caseware.com>
2020-10-18 14:31:22 -0400

    Add hover, focus, active and disabled states

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { defaultTheme, typeScale } from "../utils";
+import { defaultTheme, typeScale, primaryFont } from "../utils";
 
 const Button = styled.button`
   padding: 12px 24px;
@@ -7,25 +7,61 @@ const Button = styled.button`
   border-radius: 2px;
   min-width: 100px;
   cursor: pointer;
-  font-family: "Quicksand", sans-serif;
+  font-family: ${primaryFont};
+  transition: background-color 0.2s linear, color 0.2s linear;
+
+  &:hover {
+    background-color: ${defaultTheme.primaryHoverColor};
+    color: ${defaultTheme.textColorOnPrimary};
+  }
+
+  &:focus {
+    outline: 3px solid ${defaultTheme.primaryHoverColor};
+    outline-offset: 2px;
+  }
+
+  &:active {
+    background-color: ${defaultTheme.primaryActiveColor};
+    border-color: ${defaultTheme.primaryActiveColor};
+    color: ${defaultTheme.textColorOnPrimary};
+  }
 `;
 
 const PrimaryButton = styled(Button)`
   background-color: ${defaultTheme.primaryColor};
   border: none;
   color: white;
+
+  &:disabled {
+    background-color: ${defaultTheme.disabled};
+    color: ${defaultTheme.textOnDisabled};
+    cursor: not-allowed;
+  }
 `;
 
 export const SecondaryButton = styled(Button)`
   background: none;
   border: 2px solid ${defaultTheme.primaryColor};
   color: ${defaultTheme.primaryColor};
+
+  &:disabled {
+    background: none;
+    color: ${defaultTheme.disabled};
+    cursor: not-allowed;
+    border-color: ${defaultTheme.disabled};
+  }
 `;
 
 export const TertiaryButton = styled(Button)`
   background: none;
   border: none;
   color: ${defaultTheme.primaryColor};
+
+  &:disabled {
+    background: none;
+    color: ${defaultTheme.disabled};
+    cursor: not-allowed;
+  }
 `;
 
 export default PrimaryButton;
